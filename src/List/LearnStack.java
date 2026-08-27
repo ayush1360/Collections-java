30baf3d15d3e4d751d0d2ff80e682448eb4dee99
ayushs13600 <ayushs13600@gmail.com>
2026-08-27 13:28:22 +0530

    Stack

--- src/List/LearnStack.java
+++ src/List/LearnStack.java
@@ -6,13 +6,22 @@ public class LearnStack {
 
     public static void main(String[] args) {
 
+        // create a stack
         Stack<String> stack = new Stack<>();
 
+        // add elements using push()
         stack.push("Tiger");
         stack.push("Lion");
         stack.push("Elephant");
 
         System.out.println(stack);
+
+        // to check current top element use peek()
+        System.out.println(stack.peek());
+
+        // to remove top element use pop()
+        stack.pop();
+
         System.out.println(stack.peek());
     }
 }
