9f6872670ebc9487d79ad5e83cb6041e255e03ec
ayushs13600 <ayushs13600@gmail.com>
2026-08-27 13:24:26 +0530

    ArrayList

--- src/List/LearnArrayList.java
+++ src/List/LearnArrayList.java
@@ -9,26 +9,33 @@ public class LearnArrayList {
 
         //create a ArrayList
         ArrayList<String> list = new ArrayList<>();
+
+        //add in ArrayList
         list.add("Apple");
         list.add("Banana");
         list.add("Cherry");
+
 
         ArrayList<String> list2 = new ArrayList<>();
         list2.add("mongo");
         list2.add("grape");
 
+        // add a list in current List
         list.addAll(list2);
         list.remove("Apple");
 
         System.out.println(list);
+
+        //check if list contains a element
         System.out.println(list.contains("Banana"));
 
+        // iterate over the list using for loop
         for(String i : list){
             System.out.println(i);
         }
 
+        //iterate over the list using iterator
         Iterator<String> it = list.iterator();
-
         while(it.hasNext()){
             System.out.println(it.next());
         }
